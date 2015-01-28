084010f8e2481d4de6edb723510551a65c0b32d0
WillChilds-Klein <willck93@gmail.com>
2015-01-28 18:26:44 -0500

    update git stuff again

--- source/50_git.sh
+++ source/50_git.sh
@@ -2,4 +2,5 @@
 
 alias gstat='git status'
 alias gcom='git commit -am $1'
-# alias gcomp='git commit -am $1 && git push $2'
+alias gpush='git push $1 $2'
+# alias gcomp='git commit -am $1 && git push'
