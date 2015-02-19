cf076aed25c87696efc1282610e5aa93da489cc0
WillChilds-Klein <willck93@gmail.com>
2015-02-19 07:42:56 -0500

    master

--- source/50_git.sh
+++ source/50_git.sh
@@ -1,22 +1,25 @@
 # aliases etc. for Git
 
-alias gstat="git status $@"
-alias gdiff="git diff $@"
-alias gcom="git commit -am $1"
-alias gpush="git push $@"
+alias gstat="git status"
+alias gdiff="git diff"
+alias gcom="git commit -am"
+alias gpush="git push"
 
 function gcomp() {
+    echo "USAGE: $ gcomp [\"commit message\"] [repo1] [repo2] ... "
+
     local msg="$1"
-    if [ "$#" -eq "0" ]; then
+    if [ "$#" -eq "0" ] || [ "#1" == *\"*\"* ]; then
         echo "no message specified, using default commit message"
         local msg="[auto-generated commit msg from $HOSTNAME]"
+    else
+        shift 1
     fi
 
     git add --all &&
     git commit -m "$msg" &&
-    if [ ! "$#" -eq "0" ]; then
-        shift 1;
-    fi
 
-    git push $@ ; status
+    for repo in "$@"; do
+        git push "$repo"; status
+    done
 }
