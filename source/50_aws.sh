69ac0fb58cb23c355487071bdc7005a21dec6e32
WillChilds-Klein <willck93@gmail.com>
2015-03-13 12:51:18 -0400

    update aws env vars

--- source/50_aws.sh
+++ source/50_aws.sh
@@ -2,10 +2,14 @@
 export PATH=/usr/local/aws/bin:$PATH
 
 # export env vars
-export AWS_ACCESS_KEY_ID=
-export AWS_SECRET_ACCESS_KEY=
-export AWS_DEFAULT_REGION=us-west-2
 export AWS_CONFIG_FILE=~/.aws/config
+export AWS_DEFAULT_PROFILE=default
+#export AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)
+#export AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
+#export AWS_DEFAULT_REGION=$(aws configure get region)
 
 # set bash autocompletion
 complete -C '/usr/local/bin/aws_completer' aws
+
+# connect to zeus EC2 instance via ssh
+alias zeus='ssh -i ~/.aws/athena-zeus.pem ubuntu@ec2-52-11-189-255.us-west-2.compute.amazonaws.com'
