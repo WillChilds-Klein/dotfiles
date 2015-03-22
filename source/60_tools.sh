e66521ba73ec32e1a9315ae354cdc7a1f639432a
WillChilds-Klein <willck93@gmail.com>
2015-03-22 16:37:48 -0400

    fix some tpm issues

--- source/60_tools.sh
+++ source/60_tools.sh
@@ -21,8 +21,17 @@ function ip() {
     printf "Private IP: [${private%?}]\n"
 }
 
+# ensure tpm is there
+if ! [[ -d ~/.tmux/plugins/tpm ]]; then 
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    echo "tpm repo cloned, hit <prefix>r then <prefix>I to install plugins!"
+fi
+
 # vim-hackernews
 alias hn='vim +HackerNews'
+
+# .vimrc location
+export VIMRC="~/.vimrc"
 
 # print tmux colors
 alias tmux-colors='for i in {0..255} ; do printf "\x1b[38;5;${i}mcolour${i}\n"; done'
